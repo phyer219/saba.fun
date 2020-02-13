570d1822f0c325883ac19251122c0fe3512a0389
ZQW <phyer219@gmail.com>
2020-02-13 17:25:23 +0800

    .

--- index/views.py
+++ index/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
-=======
 
 def index(request):
     context = {'a': 'a'}
     return render(request,'index/index.html', context)
->>>>>>> 2374d35dcde45539417b1c48bd4b8945264aa13e
