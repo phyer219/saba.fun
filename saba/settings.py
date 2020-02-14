c003ac181abb6b53530b31c69ff72b347294d8a5
ZQW <phyer219@gmail.com>
2020-02-15 00:48:24 +0800

    add blog app

--- saba/settings.py
+++ saba/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # my apps
     'index',
     'poetry',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- saba/urls.py
+++ saba/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
     path('poetry/', include('poetry.urls')),
+    path('blog/', include('blog.urls')),
 ]
